aa8f1c2fe4adc77ef99f9f25043b38780aea741a
José Joaquín Atria <jjatria@gmail.com>
2017-08-18 11:06:07 +0100

    No need for global enclosure

--- t/example/endless.pl
+++ t/example/endless.pl
@@ -18,10 +18,6 @@ my $mpd = Net::Async::MPD->new(
 # use Log::Any::Adapter;
 # Log::Any::Adapter->set( 'Stderr', log_level => 'trace' );
 
-my $total_length = 21;
-my $n = 1;
-my $previous;
-
 my @all_files;
 $mpd->send( { parser => 'none' }, 'list_all', sub {
   @all_files = map { (split /:\s+/, $_, 2)[1] }
@@ -29,61 +25,62 @@ $mpd->send( { parser => 'none' }, 'list_all', sub {
     @{ shift() };
 })->get;
 
-{
-  my $future;
-  my $repeat = 1;
+my $total_length = 21;
+my $n = 1;
+my $previous;
+my $future;
+my $repeat = 1;
 
-  $mpd->on( close => sub {
-    $repeat = 0;
-    $future->done;
-  });
+$mpd->on( close => sub {
+  $repeat = 0;
+  $future->done;
+});
 
-  sub make_chain {
-    $future = $mpd->send( idle => 'player' )
-      ->then( sub {
-        return $mpd->send( 'status' );
-      })
-      ->then( sub {
-        my $status = shift;
-        my $current = $status->{songid};
-        $previous = $current unless defined $previous;
-        if ($current ne $previous) {
-          $previous = $current;
-          return $mpd->send( 'playlist' );
-        }
-        else {
-          return $loop->new_future->fail(1);
-        }
-      })
-      ->then( sub {
-        my @playlist = @{ shift() };
+sub make_chain {
+  $future = $mpd->send( idle => 'player' )
+    ->then( sub {
+      return $mpd->send( 'status' );
+    })
+    ->then( sub {
+      my $status = shift;
+      my $current = $status->{songid};
+      $previous = $current unless defined $previous;
+      if ($current ne $previous) {
+        $previous = $current;
+        return $mpd->send( 'playlist' );
+      }
+      else {
+        return $loop->new_future->fail(1);
+      }
+    })
+    ->then( sub {
+      my @playlist = @{ shift() };
 
-        my $all_new = 1;
-        my @new;
-        foreach (0..100) {
-          my @indeces = shuffle( 0..$#all_files );
-          @new = @all_files[ @indeces[ 0 .. $n-1 ] ];
+      my $all_new = 1;
+      my @new;
+      foreach (0..100) {
+        my @indeces = shuffle( 0..$#all_files );
+        @new = @all_files[ @indeces[ 0 .. $n-1 ] ];
 
-          my @diff = array_minus( @new, @playlist );
-          if (scalar @diff eq $n) { last }
-          else { $all_new = 0 }
-        }
+        my @diff = array_minus( @new, @playlist );
+        if (scalar @diff eq $n) { last }
+        else { $all_new = 0 }
+      }
 
-        warn 'Some of the added songs already exist in the playlist!'
-          unless $all_new;
+      warn 'Some of the added songs already exist in the playlist!'
+        unless $all_new;
 
-        my $end = scalar @playlist;
-        my @commands = map { [ addid => $_, $end++ ] } @new;
-        push @commands, [ delete => "0:$n" ];
+      my $end = scalar @playlist;
+      my @commands = map { [ addid => $_, $end++ ] } @new;
+      push @commands, [ delete => "0:$n" ];
 
-        return $mpd->send( \@commands );
-      })
-      ->catch(sub {
-        $loop->new_future->done
-      });
+      return $mpd->send( \@commands );
+    })
+    ->catch(sub {
+      $loop->new_future->done
+    });
 
-    return $future;
-  }
-
-  while ($repeat) { make_chain()->get }
+  return $future;
 }
+
+while ($repeat) { make_chain()->get }
